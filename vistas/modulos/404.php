46a443de959d52c91f7daadf2ae4957cb78ebeef
InformaticaSegob <dep.informatica.segob@gmail.com>
2022-08-01 12:40:27 -0500

    Seccion4 primer commit

---
+++ vistas/modulos/404.php
@@ -0,0 +1,40 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Pagina no encontrada
+        
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Pagina no encontrada</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="error-page">
+
+            <h2 class="headline text-primary">404</h2>
+            <div class="error-contet">
+                <h3>
+                    <i class="fa fa-warning text-primary"></i>
+                    Ooop! Pagina no encontrada
+                
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Nesciunt odio reiciendis in blanditiis laudantium id labore 
+                    modi ad magni nam a, illo qui magnam sint aliquam ab consequuntur error molestiae.
+                    <a href="inicio"> click aqui.</a>
+                </p>
+            </div>
+
+        </div>
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>>Copyrigth &copy; 2017 
+        <a href="https://www.duvalin.com.mx" target="blank">
+        </a>.
+    </strong>
+    Todos los Derechos Reservados.
+</footer>
