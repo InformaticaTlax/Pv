e05b023f41fea51c6363de0d4fe392764e8632e0
InformaticaSegob <dep.informatica.segob@gmail.com>
2023-03-28 17:47:45 -0600

    se termino video 119

--- controladores/ventas.controlador.php
+++ controladores/ventas.controlador.php
@@ -1,5 +1,11 @@
 <?php
 
+//require __DIR__ . '/ticket/autoload.php'; //Nota: si renombraste la carpeta a algo diferente de "ticket" cambia el nombre en esta línea
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+
 class ControladorVentas{
 
 	/*=============================================
@@ -98,6 +104,87 @@ class ControladorVentas{
 			$respuesta = ModeloVentas::mdlIngresarVenta($tabla, $datos);
 
 			if($respuesta == "ok"){
+
+				//usar impresora
+				// $impresora = "HP LaserJet Pro MFP M521 PCL 6";
+
+				// $conector = new WindowsPrintConnector($impresora);
+
+				// $imprimir = new Printer($conector);
+
+				// $imprimir -> text("hola mundo"."\n");
+
+				// $imprimir -> cut();
+				// $imprimir -> close();
+
+				$impresora = "HP LaserJet Pro MFP M521 PCL 6";
+
+				$conector = new WindowsPrintConnector($impresora);
+
+				$printer = new Printer($conector);
+
+				$printer -> setJustification(Printer::JUSTIFY_CENTER);
+
+				$printer -> text(date("Y-m-d H:i:s")."\n");//Fecha de la factura
+
+				$printer -> feed(1); //Alimentamos el papel 1 vez*/
+
+				$printer -> text("Dulceria Duvalin"."\n");//Nombre de la empresa
+
+				$printer -> text("NIT: XXXXX"."\n");//Nit de la empresa
+
+				$printer -> text("Dirección: Manuel Saldaña sur 37"."\n");//Dirección de la empresa
+
+				$printer -> text("Teléfono: 246 xxx xx xx"."\n");//Teléfono de la empresa
+
+				$printer -> text("FACTURA N.".$_POST["nuevaVenta"]."\n");//Número de factura
+
+				$printer -> feed(1); //Alimentamos el papel 1 vez*/
+
+				$printer -> text("Cliente: ".$traerCliente["nombre"]."\n");//Nombre del cliente
+
+				$tablaVendedor = "usuarios";
+				$item = "id";
+				$valor = $_POST["idVendedor"];
+
+				$traerVendedor = ModeloUsuarios::mdlMostrarUsuarios($tablaVendedor, $item, $valor);
+
+				$printer -> text("Vendedor: ".$traerVendedor["nombre"]."\n");//Nombre del vendedor
+
+				$printer -> feed(1); //Alimentamos el papel 1 vez*/
+
+				foreach ($listaProductos as $key => $value) {
+
+					$printer->setJustification(Printer::JUSTIFY_LEFT);
+
+					$printer->text($value["descripcion"]."\n");//Nombre del producto
+
+					$printer->setJustification(Printer::JUSTIFY_RIGHT);
+
+					$printer->text("$ ".number_format($value["precio"],2)." Und x ".$value["cantidad"]." = $ ".number_format($value["total"],2)."\n");
+
+				}
+				$printer -> feed(1); //Alimentamos el papel 1 vez*/			
+				
+				$printer->text("NETO: $ ".number_format($_POST["nuevoPrecioNeto"],2)."\n"); //ahora va el neto
+
+				$printer->text("IMPUESTO: $ ".number_format($_POST["nuevoPrecioImpuesto"],2)."\n"); //ahora va el impuesto
+
+				$printer->text("--------\n");
+
+				$printer->text("TOTAL: $ ".number_format($_POST["totalVenta"],2)."\n"); //ahora va el total
+
+				$printer -> feed(1); //Alimentamos el papel 1 vez*/	
+
+				$printer->text("Muchas gracias por su compra"); //Podemos poner también un pie de página
+
+				$printer -> feed(3); //Alimentamos el papel 3 veces*/
+
+				$printer -> cut(); //Cortamos el papel, si la impresora tiene la opción
+
+				$printer -> pulse(); //Por medio de la impresora mandamos un pulso, es útil cuando hay cajón moneder
+
+				$printer -> close();
 
 				echo'<script>
 
